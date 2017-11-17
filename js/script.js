e3b36430e8313edaa6670c4ac0dfa3f52bbd056f
Rituj Kumar <ritujkumar@hotmail.com>
2017-11-17 17:17:22 +0530

    adding buttons

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ function checkLoginState() {
 
 function testAPI() {
   console.log('Welcome!  Fetching your information.... ');
-  FB.api('/me', function(response) {
+  FB.api('/me', function (response) {
       console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
